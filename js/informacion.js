bcbdbcae7223c0fab54a0ddf72d8499ff194f284
Carlos Eduardo Cruz <121514453+EduardoCR91@users.noreply.github.com>
2025-04-20 19:30:48 +0000

    Creacion pagina de informacion y estilos

---
+++ js/informacion.js
@@ -0,0 +1,17 @@
+export function informacion(){
+    const contenedor = document.querySelector(".contenedor")
+
+    contenedor.innerHTML = `
+            <div class="fondo-borroso"></div>
+    <div class="contenedor">
+        <h1>Meteo Api</h1>
+        <h3>Carlos Eduardo Cruz</h3>
+        <img src="/storm.png" alt="clima">
+        <div class="texto">
+            <p>Api con informacion del clima de 75 ciudades</p>
+        </div>
+       <a href="https://github.com/EduardoCR91">github.com/EduardoCR91 v 1.0.1</a> 
+    </div>
+    
+    `;
+}
