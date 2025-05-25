bb714c8975044b97ee6b75145154f89a3ac92129
Carlos Eduardo Cruz <121514453+EduardoCR91@users.noreply.github.com>
2025-05-25 16:15:24 +0000

    ajuste en service worker con espacios

--- js/app.js
+++ js/app.js
@@ -52,7 +52,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('service-worker.js')
       .then(reg => console.log('Service Worker registrado', reg))
       .catch(err => console.log('Error registrando Service Worker', err));
   });
